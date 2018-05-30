a7e5a8002bd0d1bbd3058d0aa6f9475c0b75ab39
Sean Conrad <seanconrad123@gmail.com>
2018-05-30 23:32:58 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif speed > 40 && speed < 60
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+		if speed > 60
+			return true
+		elsif speed < 40
+			return true
+		elsif speed > 40 && speed < 60
+			return false
+		end
 end
-	
-
-
